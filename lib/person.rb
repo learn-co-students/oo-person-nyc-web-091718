3c9824f457e1f66f9d9cbdc68fe737855e94fffd
mirageywu <yashih.wu@gmail.com>
2018-09-19 17:17:47 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,86 @@
-# your code goes here
+
+
+class Person
+attr_reader :name, :happiness, :hygiene
+attr_accessor :bank_account
+
+def initialize(name)
+  @name = name
+  @bank_account = 25
+  @happiness = 8
+  @hygiene = 8
+end
+
+def happiness=(happiness_score)
+ if happiness_score >= 10
+   @happiness = 10
+ elsif happiness_score < 0
+   @happiness = 0
+ else
+   @happiness = happiness_score
+ end #happiness
+end
+
+def hygiene=(hygiene_score)
+ if hygiene_score >= 10
+   @hygiene = 10
+ elsif hygiene_score < 0
+   @hygiene  = 0
+ else
+   @hygiene = hygiene_score
+ end #hygiene
+end
+
+def happy?
+  if self.happiness > 7
+    true
+  else
+    false
+  end
+end
+
+def clean?
+  if self.hygiene > 7
+    true
+  else
+    false
+  end
+end
+
+def get_paid(salary)
+  @bank_account += salary
+  "all about the benjamins"
+end
+
+def take_bath
+ self.hygiene += 4
+   "♪ Rub-a-dub just relaxing in the tub ♫"
+ end
+
+ def work_out
+   self.hygiene -= 3
+   self.happiness += 2
+   "♪ another one bites the dust ♫"
+ end
+
+ def call_friend(friend)
+   self.happiness += 3
+   friend.happiness += 3
+   "Hi #{friend.name}! It's #{@name}. How are you?"
+ end
+
+ def start_conversation(friend, topic)
+   if topic.downcase == "politics"
+     self.happiness -= 2
+     friend.happiness -= 2
+     "blah blah partisan blah lobbyist"
+   elsif topic.downcase == "weather"
+     self.happiness += 1
+     friend.happiness += 1
+     "blah blah sun blah rain"
+   else
+     "blah blah blah blah blah"
+   end
+ end
+
+end #class Person end
